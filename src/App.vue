0f3d25afe85ee20a6f2269dbba63a328376192cf
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2020-07-03 18:34:42 +0300

    Add logo

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div id="app">
-    <nav class="nav">
-      <div class="nav-left">
-        <a class="brand" href="#">Brand</a>
-
-        <div class="tabs">
-          <router-link exact-active-class="active" to="/">Exchanges</router-link>
-          <router-link exact-active-class="active" to="/markets">Markets</router-link>
-          <router-link exact-active-class="active" to="/trades">Trades</router-link>
-          <!--<router-link exact-active-class="active" to="/about">About</router-link>-->
-        </div>
-      </div>
-
-      <div class="nav-right">
-        <a class="button outline">Button</a>
-      </div>
-    </nav>
+    <Nav/>
 
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-</style>
+<script>
+import Nav from '@/components/Nav'
+
+export default {
+  name: 'App',
+  components: {
+    Nav
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {
+  },
+  methods: {}
+}
+</script>
+
---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="nav">
+    <div class="nav-left">
+      <router-link
+        class="brand"
+        to="/">
+        <wolfIcon width="30px"/>
+      </router-link>
+
+      <div class="tabs">
+        <router-link exact-active-class="active" to="/">Exchanges</router-link>
+        <router-link exact-active-class="active" to="/markets">Markets</router-link>
+        <router-link exact-active-class="active" to="/trades">Trades</router-link>
+        <!--<router-link exact-active-class="active" to="/about">About</router-link>-->
+      </div>
+    </div>
+
+    <div class="nav-right">
+      <a class="button outline">Button</a>
+    </div>
+  </nav>
+</template>
+
+<style>
+  .nav {
+    margin-bottom: 16px;
+  }
+</style>
+
+<script>
+import wolfIcon from '@/assets/svg/wolf-pack-battalion.svg'
+
+export default {
+  name: 'Nav',
+  components: {
+    wolfIcon
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {
+  },
+  methods: {}
+}
+</script>
