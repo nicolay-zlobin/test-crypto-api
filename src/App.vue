a79f7b76b6fdb3733415458251e6fc8f70e85668
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2020-07-07 17:49:48 +0300

    Nav for small screens

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="mt-10">
     <Nav/>
 
     <router-view/>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,11 +9,17 @@
         </router-link>
 
         <div class="tabs">
-          <router-link exact-active-class="active" to="/">Exchanges</router-link>
-          <router-link active-class="active" to="/markets">Markets</router-link>
-          <router-link active-class="active" to="/trades">Trades</router-link>
-          <router-link active-class="active" to="/chart">Chart</router-link>
-          <router-link active-class="active" to="/about">About</router-link>
+          <caretDown
+            class="tabs-toggler is-pointer"
+            width="22px"
+            @click="navIsVisible = !navIsVisible"/>
+          <template v-if="navIsVisible">
+            <router-link exact-active-class="active" to="/">Exchanges</router-link>
+            <router-link active-class="active" to="/markets">Markets</router-link>
+            <router-link active-class="active" to="/trades">Trades</router-link>
+            <router-link active-class="active" to="/chart">Chart</router-link>
+            <router-link active-class="active" to="/about">About</router-link>
+          </template>
         </div>
       </div>
 
@@ -24,31 +30,56 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
   .nav {
     margin-bottom: 16px;
+    .nav-right {
+      margin-right: 16px;
+      align-items: center;
+      margin-left: 20px;
+    }
+    @media screen and (max-width: 480px) {
+      .nav-right {
+        display: none;
+      }
+    }
   }
-  .nav-right {
-    margin-right: 16px;
-    align-items: center;
+  .tabs {
+    .tabs-toggler {
+      padding: 2px;
+    }
+    @media screen and (max-width: 479px) {
+      flex-direction: column;
+      width: 100%;
+    }
+    @media screen and (min-width: 480px) {
+      .tabs-toggler {
+        display: none;
+      }
+    }
   }
 </style>
 
 <script>
 import wolfIcon from '@/assets/svg/wolf-pack-battalion.svg'
 import ThemeSwitch from '@/components/ThemeSwitch'
+import caretDown from '@/assets/svg/caret-square-down.svg'
 
 export default {
   name: 'Nav',
   components: {
     wolfIcon,
-    ThemeSwitch
+    ThemeSwitch,
+    caretDown
   },
   data () {
-    return {}
+    return {
+      navIsVisible: window.screen.width > 479
+    }
   },
   computed: {},
   mounted () {
+    console.log(window.screen.width)
   },
   methods: {}
 }
